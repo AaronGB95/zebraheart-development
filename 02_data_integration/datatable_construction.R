45c85c431f92e5b49da0091467b572dbbf103fdb
agarblaz <aaron.garcia.blazquez@gmail.com>
2023-09-28 15:00:06 +0200

    Added counts files

--- 02_data_integration/datatable_construction.R
+++ 02_data_integration/datatable_construction.R
@@ -32,23 +32,18 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 ## Data Load
 ##-----------------------------------------------------------------------------
 # Get featureCounts output files
-files <- list.files(path = getwd(), pattern = "counts.txt", full.names = TRUE)
+files <- list.files(path = "./counts/", full.names = TRUE)
 
 # Loop through each file and read it into a data frame
-for (i in seq_along(files)) {
-  load(files[i])
-}
+datalist <- lapply(files, FUN = read.table, header=TRUE)
 ##-----------------------------------------------------------------------------
 
 
 ##-----------------------------------------------------------------------------
 ## Gene Names
 ##-----------------------------------------------------------------------------
-# Save counts into list of objects
-datafiles <- ls(pattern = "counts")
-
 # Get gene names
-names <- lapply(datafiles, function(x) rownames(get(x)))
+names <- lapply(datalist, "[", , "Geneid")
 
 # Make intersection of gene names
 names <- Reduce(unique, names)
@@ -58,13 +53,14 @@ names <- Reduce(unique, names)
 ##-----------------------------------------------------------------------------
 ## Gene Counts
 ##-----------------------------------------------------------------------------
-datamatrix <- matrix(NA, nrows = length(names), ncols = length(datafiles))
+datamatrix <- matrix(NA, nrow = length(names), ncol = length(datalist))
 rownames(datamatrix) <- names
-colnames(datamatrix) <- strsplit(files, split = "_counts.txt")
+
+colnames(datamatrix) <- grepl(pattern = "(?<=/)[^/]*(?=_)", x = files)
 
 # Loop through files and merge all counts into a single table
-for (fi in datafiles) {
-  datamatrix[, fi] <- fi[, -1]
+for (fi in datalist) {
+  datamatrix[, fi] <- fi[, 7]
 }
 ##-----------------------------------------------------------------------------
 
